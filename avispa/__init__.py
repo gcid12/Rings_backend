64e57bedcaf13acd7feb12b217bff6a8bb569713
RCid <rcid@myring.io>
2014-08-25 12:41:50 -0400

    This closes #3

--- avispa/__init__.py
+++ avispa/__init__.py
@@ -12,3 +12,9 @@ avispa.config.from_object('config')
 @avispa.errorhandler(404)
 def not_found(error):
     return render_template('404.html'), 404
+
+# Import a module / component using its blueprint handler variable (mod_auth)
+from avispa.avispa_web.controllers import avispa_web
+
+# Register blueprint(s)
+avispa.register_blueprint(avispa_web)
