6cfb9c62e8fd57e2307345a932996b66ef1c76d0
RCid <rcid@myring.io>
2014-11-05 14:12:58 -0600

    Moved Server() to __init__

--- avispa/avispa_rest/AvispaCouchDB.py
+++ avispa/avispa_rest/AvispaCouchDB.py
@@ -5,11 +5,24 @@ import os
 
 class AvispaCouchDB:
 
+    def __init__(self):
+
+        self.couch = couchdb.Server()
+
     def _instantiate_couchdb_as_admin(self):
 
-        couch = couchdb.Server()
+        
         # couch.resource.credentials = (os.environ['MYRING_COUCH_DB_USER'], 
-        #	                          os.environ['MYRING_COUCH_DB_PASS'])
-        couch.resource.credentials = (u'admin',u'happy123')
+        #                             os.environ['MYRING_COUCH_DB_PASS'])
+        self.couch.resource.credentials = (u'admin',u'happy123')
         #print couch.resource.credentials
-        return couch 
+        return self.couch 
+
+    def _instantiate_couchdb_as_user(self,username=None,password=None):
+
+        if username and password:
+            self.couch.resource.credentials = (username,password)
+
+        couch = couchdb.Server()
+
+        return self.couch 
