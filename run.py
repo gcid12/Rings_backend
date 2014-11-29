9775a429d914f4a343e9fcf2db525ebf6b8b0a4b
RCid <rcid@myring.io>
2014-11-29 15:48:03 -0600

    0.0.0.0 in run.py

--- run.py
+++ run.py
@@ -10,5 +10,5 @@ app.register_blueprint(auth_flask_login)
 
 if __name__ == "__main__":
 	#avispa.run(host='0.0.0.0', port=80)
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
     
