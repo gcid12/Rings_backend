3b608be080584077d0086b0da816e8baf9747167
root <root@cs2.apiring.com>
2014-11-29 15:11:08 -0600

    adjustments to make Production and Dev compatible

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 # Run a test server.
-from avispa import avispa
+from app import app
 
 # Import a module / component using its blueprint handler variable (mod_auth)
 from avispa.avispa_rest.controllers import avispa_rest
+from auth.auth import auth_flask_login
 # Register blueprint(s)
-avispa.register_blueprint(avispa_rest)
+app.register_blueprint(avispa_rest)
+app.register_blueprint(auth_flask_login)
 
 if __name__ == "__main__":
 	#avispa.run(host='0.0.0.0', port=80)
-    avispa.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='127.0.0.1', port=8080, debug=True)
     
--- x_run.py
+++ x_run.py
@@ -1,14 +1,12 @@
 # Run a test server.
-from app import app
+from avispa import avispa
 
 # Import a module / component using its blueprint handler variable (mod_auth)
 from avispa.avispa_rest.controllers import avispa_rest
-from auth.auth import auth_flask_login
 # Register blueprint(s)
-app.register_blueprint(avispa_rest)
-app.register_blueprint(auth_flask_login)
+avispa.register_blueprint(avispa_rest)
 
 if __name__ == "__main__":
 	#avispa.run(host='0.0.0.0', port=80)
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    avispa.run(host='127.0.0.1', port=8080, debug=True)
     
