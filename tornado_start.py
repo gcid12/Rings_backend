7edc983e9382b099b4781a86f8067c7343ec85b7
RCid <rcid@myring.io>
2016-02-03 05:57:34 -0500

    Tornado server config

---
+++ tornado_start.py
@@ -0,0 +1,26 @@
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+from run import app
+
+def main():
+
+	http_server = HTTPServer(WSGIContainer(app))
+
+
+	# One Process
+	#http_server.listen(5000)
+
+	
+	# Multi Process
+	http_server.bind(5000)
+	http_server.start(0) # forks one process per cpu
+	
+
+	IOLoop.instance().start()
+
+
+
+if __name__ == '__main__':
+    main()
+
