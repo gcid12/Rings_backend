5dbf0fde1840229584da40af5dcadbe358f40654
RCid <rcid@myring.io>
2016-04-30 00:14:01 -0400

    depend on life/ scripts

--- tornado_start.sh
+++ tornado_start.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-killall python
-deactivate
-source env/bin/activate
-nohup python tornado_start.py &
+#killall python
+#deactivate
+#source env/bin/activate
+#nohup python tornado_start.py &
 
-sudo service nginx restart
+#sudo service nginx restart
+
+#Stop server
+source life/server_stop.sh
+#Stop application
+source life/app_stop.sh
+#Start application
+source life/app_start.sh
+#Start server
+source life/server_start.sh
+
