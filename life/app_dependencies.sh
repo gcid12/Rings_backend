5ce8feb219c5d41445f4cdfda5dbcc63eed73e1c
RCid <rcid@myring.io>
2016-04-30 00:13:19 -0400

    Not reading from requirements.txt

--- life/app_dependencies.sh
+++ life/app_dependencies.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-virtualenv --no-site-packages --distribute env && source env/bin/activate && pip install -r requirements.txt
+#virtualenv --no-site-packages --distribute env && source env/bin/activate && pip install -r requirements.txt
+
+virtualenv --no-site-packages --distribute env && source env/bin/activate
+pip install flask
+pip install flask-login
+pip install flask-openid
+pip install flask-bcrypt
