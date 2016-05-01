de02e3caab5b4430251dd7a56b9c556360b6b94f
RCid <rcid@myring.io>
2016-05-01 18:55:16 -0400

    Add elastic search to app dependency

--- life/app_dependencies.sh
+++ life/app_dependencies.sh
@@ -11,6 +11,7 @@ pip install flask-bcrypt
 pip install tornado
 pip install requests
 pip install couchdb
+pip install elasticsearch
 
 aws s3 cp s3://myring-infra/avispa/20160501/env_config.py env_config.py
 id
