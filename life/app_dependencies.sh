547542d4a3ec7508097c63587b015b5fbe8ab44f
RCid <rcid@myring.io>
2016-05-01 18:39:23 -0400

    add couchdb library

--- life/app_dependencies.sh
+++ life/app_dependencies.sh
@@ -10,6 +10,7 @@ pip install flask-openid
 pip install flask-bcrypt
 pip install tornado
 pip install requests
+pip install couchdb
 
 aws s3 cp s3://myring-infra/avispa/20160501/env_config.py env_config.py
 id
