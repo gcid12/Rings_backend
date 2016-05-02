70020816569f2720a5bfeecfb230ccab7d3d0f5d
RCid <rcid@myring.io>
2016-05-01 20:13:31 -0400

    added elasticsearch_dsl to app dependencies

--- life/app_dependencies.sh
+++ life/app_dependencies.sh
@@ -12,6 +12,7 @@ pip install tornado
 pip install requests
 pip install couchdb
 pip install elasticsearch
+pip install elasticsearch_dsl
 
 aws s3 cp s3://myring-infra/avispa/20160501/env_config.py env_config.py
 id
