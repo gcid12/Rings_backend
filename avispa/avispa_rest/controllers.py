9228f2825cdd127ee3640b3cece5bd4ae46dd634
RCid <rcid@myring.io>
2016-05-08 13:36:18 -0400

    Print token values

--- avispa/avispa_rest/controllers.py
+++ avispa/avispa_rest/controllers.py
@@ -69,7 +69,11 @@ def route_dispatcher(depth,handle,ring=None,idx=None,api=False,collection=None):
     if api:
         
         # PLEASE REMOVE TEMP_ACCESS_TOKEN with real OAuth access token  ASAP!
-        if request.headers.get('access_token')== TEMP_ACCESS_TOKEN:
+        lggr.info('TEMP_ACCESS_TOKEN:'+str(TEMP_ACCESS_TOKEN))
+        lggr.info(request.headers.get('access_token'))
+
+        if request.headers.get('access_token') == TEMP_ACCESS_TOKEN:
+            lggr.info('TEMP_ACCESS_TOKEN matches!')
             user_handle = '_api_anonymous' #Please change this to the actual username that is using this token
         else:
             return render_template('avispa_rest/error_401.html', data=data),401
