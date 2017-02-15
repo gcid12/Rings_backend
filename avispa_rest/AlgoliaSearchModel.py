b52d2f272af21010cbda8cb270b0c5d4ef1248e7
RCid <rcid@myring.io>
2017-02-14 21:20:07 -0500

    Algolia restful object for Avispa.

--- avispa_rest/AlgoliaSearchModel.py
+++ avispa_rest/AlgoliaSearchModel.py
@@ -1,7 +1,10 @@
+#ALGOLIA
 from env_config import ALGOLIA_APPLICATION_ID,ALGOLIA_ADMIN_API_KEY
+import algoliasearch
 
 #LOGGING
 import logging
+import requests
 from AvispaLogging import AvispaLoggerAdapter
 
 
@@ -15,26 +18,32 @@ class AlgoliaSearchModel:
         client = algoliasearch.Client(ALGOLIA_APPLICATION_ID, ALGOLIA_ADMIN_API_KEY)
         indexname = '%-%'%(handle,ring)
         self.index = client.init_index(indexname)
+
+
+    def post_item(self,content):
+
+        r = self.index.add_object(content) 
+        return check_status(r.status_code)
+
+        
+    def put_item(self,content,idx):
+
+        object_id = '<idx>'
+        r = self.index.add_object(content, object_id=idx)
+        return check_status(r.status_code)
         
 
+    def delete_item(self,idx):
 
-    def index_item(self,handle,ring,idx):
+        r = self.index.delete_object(self, idx)
+        return check_status(r.status_code)
 
-    	
+    def check_status(self,status)
+
+        if r.status_code[0]=='2': 
+            return True
+        else:
+            return False
         
-    	
-
-    def unindex_item(self,handle,ring,idx):
-    	
-    	
-        
-
-    def config_index:
-    	
-    	
-        
-
-    
-
 
 
